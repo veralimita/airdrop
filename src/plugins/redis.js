3d094a8a3302a5918f76f431eab06ed10e0a3b36
Landgraf Paul <landgraf.paul@gmail.com>
2018-04-18 17:39:43 +0200

    reconnect redis

--- src/plugins/redis.js
+++ src/plugins/redis.js
@@ -14,6 +14,17 @@ class Redis {
 			process.env.REDIS_PORT_6379_TCP_PORT,
 			process.env.REDIS_PORT_6379_TCP_ADDR
 		);
+
+		client.on('error', (err) => {
+			//console.error("Redis ERROR", err.message);
+		});
+
+		client.once('end', () => {
+			setTimeout(() => {
+				this.createClient(db, cb);
+			}, 1000);
+		});
+
 		client.select(db, () => {
 			cb && cb(null, client)
 		})
