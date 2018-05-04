362ff9823596c4c3311995b990c01ee289a80a20
vera.limita@utopiamusic.com <E2hEr4nDVF4o>
2018-05-04 15:03:12 +0200

    api bot - remove referral api

--- src/modules/api-bot.js
+++ src/modules/api-bot.js
@@ -56,60 +56,6 @@ module.exports = function () {
 		});
 	});
 
-	router.post('/referral', (req, res) => {
-		if (!req.body || !req.body.source || !req.body.referral || ['rocketchat', 'telegram'].indexOf(req.body.source) === -1) {
-			return res.sendStatus(500);
-		}
-
-		async.autoInject({
-			code: (cb) => {
-				this[req.body.source].getUser(req.body.payload.id, (error, result) => {
-					if (error || !result) {
-						return cb(error || `${req.body.source} user doesn't exist`)
-					}
-					cb(null, result)
-				});
-			},
-			wallet: (code, cb) => {
-				this.wallet.getWallet(code, (error, response) => {
-					cb(error, response)
-				})
-			},
-			referral: (wallet, cb) => {
-				if (wallet.refer === req.body.referral) {
-					return setImmediate(cb, 'Referral emitted by this account')
-				}
-				if (!!wallet.referral) {
-					return setImmediate(cb, 'Referral was register for this wallet')
-				}
-				this.refer.get(req.body.referral, (err, response) => {
-					if (err || !response) {
-						return cb(err || 'Wrong referral code')
-					}
-					cb(null, response)
-				})
-			},
-			checkReferral: (referral, wallet, cb) => {
-				this.refer.canUse(req.body.referral, (err) => {
-					if (err) {
-						return cb(err)
-					}
-					cb(null, true)
-				})
-			},
-			apply: (checkReferral, wallet, cb) => {
-				this.refer.use(req.body.referral, wallet, cb)
-			}
-		}, (error, scope) => {
-			if (error) {
-				res.status(500).send(error)
-			}
-			else {
-				res.send({response: OK});
-			}
-		});
-	});
-
 	this.express.use('/bot', router);
 
 	return Promise.resolve();
